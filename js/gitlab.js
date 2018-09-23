15d4a80d171584e225a47c1487090cfb1b8610f7
Samuel Elliott <samuel@fancy.org.uk>
2018-09-24 00:09:04 +0100

    Add Discord VIPs and show star counts for projects on GitLab

--- js/gitlab.js
+++ js/gitlab.js
@@ -1,6 +1,7 @@
-$(document).ready($.refreshstars = function() {
-    $('.github-stars').each(function() {
+$(document).ready($.refreshgitlabstars = function() {
+    $('.gitlab-stars').each(function() {
         var $this = $(this),
+            host = $this.attr('data-gitlab-server') || 'gitlab.com',
             repository = $this.attr('data-repository'),
             content = $this.attr('data-updatingcontent'),
             beforecontent = $this.attr('data-beforecontent') || '',
@@ -11,13 +12,13 @@ $(document).ready($.refreshstars = function() {
             $this.html(content);
 
         $.ajax({
-            url: 'https://api.github.com/repos/' + repository,
+            url: 'https://' + host + '/api/v4/projects/' + repository,
             method: 'GET',
             success(response) {
                 if (typeof response == 'string')
                     response = $.parseJSON(response);
 
-                $this.html(beforecontent + response.stargazers_count + aftercontent);
+                $this.html(beforecontent + response.star_count + aftercontent);
             },
             error() {
                 $this.html(errorcontent);
